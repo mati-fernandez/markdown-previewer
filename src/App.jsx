5a316fc05999423acf94c9a1d010819214470092
Matias Fernandez <matifer.dev@gmail.com>
2024-03-14 15:34:10 -0300

    Almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 
 function App() {
-  const [editor, setEditor] = useState('# Hola\n\nRendered by **marked**.');
+  const defaultState = `
+# Header 1
+
+## Header 2
+
+[Link Example](https://www.example.com)
+
+\`Inline code example\`
+
+~~~
+// Code block example
+function helloWorld() {
+  console.log('Hello, World!');
+}
+~~~
+
+- List item 1
+- List item 2
+
+> Blockquote example
+
+![Image Example](https://media.istockphoto.com/id/471621500/photo/tennis-court-with-tennis-ball-close-up.jpg?s=612x612&w=0&k=20&c=K4kHMI9lxAl2L_s6CfKdR_VOHWcjx6KDHXJNRN35myc=)
+
+**Bold text example**
+`;
+
+  const [editor, setEditor] = useState(defaultState);
+
+  useEffect(() => {
+    updatePreview(editor);
+  }, [editor]);
+
+  const updatePreview = (value) => {
+    const sanitizedHtml = DOMPurify.sanitize(marked(value));
+    document.getElementById('preview').innerHTML = sanitizedHtml;
+  };
 
   const handleEditorChange = (event) => {
     setEditor(event.target.value);
   };
-  const updatePreview = () => {
-    document.getElementById('preview').innerHTML = marked(
-      document.getElementById('editor').value
-    );
-  };
-
-  updatePreview();
 
   return (
-    <>
+    <div id="container">
       <h2>Type some MD format text downhere...</h2>
-      <textarea id="editor" onChange={handleEditorChange}></textarea>
+      <textarea
+        id="editor"
+        value={editor}
+        onChange={handleEditorChange}
+      ></textarea>
       <h2>Markdown previewer</h2>
       <div id="preview"></div>
-    </>
+    </div>
   );
 }
 
